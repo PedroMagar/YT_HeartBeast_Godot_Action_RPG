feda1b78896fef9cea5a4db6fe37fdc45f3ff724
Pedro Henrique Martins Garcia <pedro.phmg@gmail.com>
2022-10-15 09:25:23 -0300

    Aula 21

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -22,6 +22,10 @@ onready var stats = $Stats
 onready var playerDetectionZone = $PlayerDetectionZone
 onready var hurtbox = $Hurtbox
 onready var softCollision = $SoftCollision
+onready var wanderController = $WanderController
+
+func _ready():
+	state = pick_random_state([IDLE, WANDER])
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
@@ -31,26 +35,52 @@ func _physics_process(delta):
 		IDLE:
 			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 			seek_player()
-		
+			
+			if wanderController.get_time_left() == 0:
+				renew_wander()
+			
 		WANDER:
-			pass
+			seek_player()
+			
+			if wanderController.get_time_left() == 0:
+				renew_wander()
+			
+			wander_to_position(wanderController.target_position,delta)
+			
+			if global_position.distance_to(wanderController.target_position) <= MAX_SPEED * delta:
+				renew_wander()
+			
 		
 		CHASE:
 			var player = playerDetectionZone.player
 			if player != null:
-				var direction = (player.global_position - global_position).normalized()
-				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
-				sprite.flip_h = velocity.x < 0
+				wander_to_position(player.global_position,delta)
 			else:
 				state = IDLE
+	
+	#sprite.flip_h = velocity.x < 0
 	
 	if softCollision.is_colliding():
 		velocity += softCollision.get_push_vector() * delta * 400
 	velocity = move_and_slide(velocity)
 
+func renew_wander():
+	state = pick_random_state([IDLE, WANDER])
+	wanderController.start_wander_timer(rand_range(1,3))
+
+func wander_to_position(position, delta):
+	#var direction = (player.global_position - global_position).normalized()
+	var direction = global_position.direction_to(position)
+	velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+	sprite.flip_h = velocity.x < 0
+
 func seek_player():
 	if playerDetectionZone.can_see_player():
 		state = CHASE
+
+func pick_random_state(state_list):
+	state_list.shuffle()
+	return state_list.pop_front()
 
 func _on_Hurtbox_area_entered(area):
 	var knockback_vector = area.knockback_vector
